e6e62fb94d145ade4a9c684441647029d5c07d83
jang95 <ruin950521@naver.com>
2024-10-26 05:56:54 +0900

    fix: cors 에러 해결 위한 test - cors의 경우 서버의 배포를 지원하는 사이트나 툴 상황에 따라 사용할 수 있거나 제한적 - cors 헤더 직접 설정

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,29 @@ import authRoutes from './routes/authRoutes';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ['https://localhost:5173', 'https://jj-movie-engine.netlify.app'],
-    credentials: true,
-  })
-);
+// cloudtype에서는 cors가 먹히지 않는가?
+// 배포하는 서비스가 서로 다를 경우 cors 사용이 제한적일 수 있다.
+// app.use(
+//   cors({
+//     origin: ['https://localhost:5173', 'https://jj-movie-engine.netlify.app'],
+//     credentials: true,
+//   })
+// );
+
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    'https://localhost:5173',
+    'https://jj-movie-engine.netlify.app',
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin as string)) {
+    res.setHeader('Access-Control-Allow-Origin', origin as string);
+  }
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(cookieParser());
 
