4cea0cca00f1b13a75dc358f9caed5a673eb3f75
jang95 <ruin950521@naver.com>
2024-10-26 06:31:07 +0900

    test: http-proxy-middleware 사용해보기

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,29 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { startServer } from './server';
 import authRoutes from './routes/authRoutes';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 
 // CORS 설정
-const corsOptions = {
-  origin: '*',
-  credentials: true, // 쿠키 전송 허용
-};
+// const corsOptions = {
+//   origin: '*',
+//   credentials: true, // 쿠키 전송 허용
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target:
+      'https://port-0-jj-movie-app-backend-m2otldplb5943039.sel4.cloudtype.app',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api': '',
+    },
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
