ea6ac75e9c650870b72c21d7761e87091b603c02
jang95 <ruin950521@naver.com>
2024-10-28 03:17:14 +0900

    Revert "test: http-proxy-middleware 사용해보기"
    This reverts commit 4cea0cca00f1b13a75dc358f9caed5a673eb3f75.

--- src/index.ts
+++ src/index.ts
@@ -3,36 +3,16 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { startServer } from './server';
 import authRoutes from './routes/authRoutes';
-import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ['https://localhost:5173', 'https://jj-movie-engine.netlify.app'], // 허용할 origin 설정
-    credentials: true,
-  })
-);
+// CORS 설정
+const corsOptions = {
+  origin: '*',
+  credentials: true, // 쿠키 전송 허용
+};
 
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(204);
-});
-
-app.use(
-  '/api',
-  createProxyMiddleware({
-    target:
-      'https://port-0-jj-movie-app-backend-m2otldplb5943039.sel4.cloudtype.app',
-    changeOrigin: true,
-    pathRewrite: {
-      '^/api': '',
-    },
-  })
-);
+app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use(express.json());
